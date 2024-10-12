3042b08df44dc6a052e81dc422b8a8a2b00dc794
IsaacScheff <isaac1996@comcast.net>
2024-10-12 16:46:09 -0400

    Moved tag argument to first spot in leafnode call

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,8 +1,8 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag = None, props = None):
-        super().__init__(value = value, tag = tag, props = props)
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
     
     def to_html(self):
         if self.value == None:
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -4,7 +4,7 @@ from leafnode import LeafNode
 
 class TestLeafNode(unittest.TestCase):    
     def test_leafnode_only_value(self):
-        node = LeafNode("example text")
+        node = LeafNode(None, "example text")
         self.assertEqual(node.__repr__(), "HTML Node: Tag-None Value-example text\nChildren-None\nProps-None")
 
     def test_leafnode_no_value(self):
@@ -12,12 +12,12 @@ class TestLeafNode(unittest.TestCase):
             LeafNode()
 
     def test_leafnode_tag_no_props(self):
-        node = LeafNode("This is a paragraph of text.", tag = "p")
+        node = LeafNode("p", "This is a paragraph of text.")
         self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
 
 
     def test_leafnode_tag_with_props(self):
-        node = LeafNode("Click me!", tag = "a", props = {"href": "https://www.google.com"})
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
 
 if __name__ == "__main__":
