dc83e2d82c7887965ee3367ade8cf77cd0a75815
IsaacScheff <isaac1996@comcast.net>
2024-10-12 21:05:22 -0400

    Conssolidated leafnode and htmlnode files

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTML Node: Tag-{self.tag} Value-{self.value}\nChildren-{self.children}\nProps-{self.props}"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("LeafNode must have value")
+        
+        if self.tag == None: 
+            return self.value
+        elif self.props == None: #tag without props
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else: #tag with props
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):    
     def test_htmlnode_no_values(self):
@@ -16,6 +16,27 @@ class TestHTMLNode(unittest.TestCase):
     def test_htmlnode_with_tag(self):
         node = HTMLNode(tag = "h1")
         self.assertEqual(node.__repr__(), "HTML Node: Tag-h1 Value-None\nChildren-None\nProps-None")
+      
+    def test_leafnode_only_value(self):
+        node = LeafNode(None, "example text")
+        self.assertEqual(node.__repr__(), "HTML Node: Tag-None Value-example text\nChildren-None\nProps-None")
+
+    def test_leafnode_no_value(self):
+        with self.assertRaises(TypeError):
+            LeafNode()
+
+    def test_leafnode_tag_no_props(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+
+
+    def test_leafnode_tag_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
+if __name__ == "__main__":
+    print("test")
+    unittest.main()
 
 if __name__ == "__main__":
     print("test")
